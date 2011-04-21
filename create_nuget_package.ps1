d18857654358a0c4986f3b2df118195a313f67e3
Tom Lokhorst <tom@lokhorst.eu>
2011-04-21 21:05:04 +0000

    Moved files around to a slightly cleaner directory structure.

--- create_nuget_package.ps1
+++ create_nuget_package.ps1
@@ -1,20 +1,24 @@
 # Quickly hacked up "build" file to create NuGet package
 
-# remove whatever was left from previous build
-remove-item -recurse -force  NuGet\lib
-remove-item -recurse -force  NuGet\tool
-
 # copy files from a succesfull Visual Studio release build
+new-item -itemtype directory  NuGet\content
 new-item -itemtype directory  NuGet\lib
 new-item -itemtype directory  NuGet\tool
 
-Copy-Item CoffeeScriptHttpHandler\bin\Release\CoffeeScriptHttpHandler.dll  NuGet\lib
-Copy-Item CoffeeScriptHttpHandler\bin\Release\CoffeeSharp.dll              NuGet\lib
-Copy-Item CoffeeScriptHttpHandler\bin\Release\Jurassic.dll                 NuGet\lib
+Copy-Item src\web.config.transform  NuGet\content
 
-Copy-Item Coffee\bin\Release\Coffee.exe       NuGet\tool
-Copy-Item Coffee\bin\Release\CoffeeSharp.dll  NuGet\tool
-Copy-Item Coffee\bin\Release\Jurassic.dll     NuGet\tool
+Copy-Item src\CoffeeScriptHttpHandler\bin\Release\CoffeeScriptHttpHandler.dll  NuGet\lib
+Copy-Item src\CoffeeScriptHttpHandler\bin\Release\CoffeeSharp.dll              NuGet\lib
+Copy-Item src\CoffeeScriptHttpHandler\bin\Release\Jurassic.dll                 NuGet\lib
+
+Copy-Item src\Coffee\bin\Release\Coffee.exe       NuGet\tool
+Copy-Item src\Coffee\bin\Release\CoffeeSharp.dll  NuGet\tool
+Copy-Item src\Coffee\bin\Release\Jurassic.dll     NuGet\tool
+
+Copy-Item src\CoffeeSharp.nuspec  NuGet
 
 # actual packaging
 tools\NuGet.exe pack NuGet\CoffeeSharp.nuspec
+
+# cleanup
+remove-item -recurse -force  NuGet
